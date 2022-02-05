86614774a540e22274386c9fc86887365bc961ef
sourabh <nick.tdr19@gmail.com>
2022-02-05 10:44:37 +0530

    Added model for unauthenticated user

---
+++ app/src/main/java/sourabh/pal/cricket/common/domain/model/auth/UnauthenticatedUser.kt
@@ -0,0 +1,8 @@
+package sourabh.pal.cricket.common.domain.model.auth
+
+data class UnauthenticatedUser(
+    private val userName: String,
+    private val email: String,
+    private val password: String,
+
+)
